15ee3aa85add1592fdaefa290885eb1e0122e37d
Jon Larrea <jon.larrea@ed.ac.uk>
2022-12-20 17:53:24 +0100

    Pantheon fixed and scripts working. TODO: Final test

--- scripts/run_pantheon.sh
+++ scripts/run_pantheon.sh
@@ -6,25 +6,36 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+if [ ! -f "/local/repository/scripts/pantheon/ready" ]; then
+    echo "Please wait, Pantheon is being installed."
+    exit 1
+fi
+
 BASE_DIR="/local/repository/scripts"
 
 cd $BASE_DIR
 
 # Generating Pantheon trace out of a PCAP trace
 mkdir -p "$BASE_DIR/tmp_pantheon_traces/"
-PCAP_BASENAME=$(basename -- '$1')
-PANTHEON_TRACE="${PCAP_BASENAME%.*}.x"
+PCAP_BASENAME=$(basename -- "$1")
+PANTHEON_TRACE=${PCAP_BASENAME%.*}.x
 ./pcap_to_pantheon.py $1 $2 tmp_pantheon_traces/$PANTHEON_TRACE
 
 # Emulate with Pantheon
 cd pantheon
 mkdir -p outputs/
-PANTHEON_OUTPUT_DIR="outputs/${PANTHEON_TRACE%.*}"
+PANTHEON_OUTPUT_DIR=$BASE_DIR/pantheon/outputs/${PANTHEON_TRACE%.*}
 rm -Rf $PANTHEON_OUTPUT_DIR 2> /dev/null
 mkdir $PANTHEON_OUTPUT_DIR
+
 CC_SCHEMES="sprout cubic verus"
-echo "Schemes used: $CC_SCHEMES"
-/src/experiments/test.py local --schemes $CC_SCHEMES --uplink-trace tmp_pantheon_traces/$PANTHEON_TRACE --data-dir $PANTHEON_OUTPUT_DIR
+
+./src/experiments/test.py local --schemes "$CC_SCHEMES" --uplink-trace ../tmp_pantheon_traces/$PANTHEON_TRACE --data-dir $PANTHEON_OUTPUT_DIR
 
 # Analyze emulation
 src/analysis/analyze.py --data-dir $PANTHEON_OUTPUT_DIR
+
+date
+echo "Pantheon emulation completed using: $CC_SCHEMES"
+echo "Run the following command to get the report:"
+echo "scp $(whoami)@$(hostname):$PANTHEON_OUTPUT_DIR/pantheon_report.pdf ."
--- scripts/setup_pantheon.sh
+++ scripts/setup_pantheon.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-if [ -d "/local/repository/scripts/pantheon" ];
-then
+if [ -f "/local/repository/scripts/pantheon/ready" ]; then
     echo "Pantheon already installed."
     cd /local/repository/scripts/pantheon
     src/experiments/setup.py --schemes "bbr copa cubic fillp fillp_sheep ledbat pcc pcc_experimental quic scream sprout taova vegas verus vivace webrtc"
+    exit 1
 fi
 
+# Install dependencies
 sudo apt update -y
-sudo apt install -y python-yaml
+sudo apt install -y python-yaml python-pip mahimahi texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
 sudo add-apt-repository -y ppa:wireshark-dev/stable
 sudo DEBIAN_FRONTEND=noninteractive apt install -y tshark
 sudo sysctl -w net.ipv4.ip_forward=1
+pip install numpy matplotlib
 
-# Install mahimahi
-sudo add-apt-repository -y ppa:keithw/mahimahi
-sudo apt -y update
-sudo apt install -y mahimahi
-
-#Dealing with Pantheon
+# Install Pantheon-tunnel
 cd /local/repository/scripts/
+git clone https://github.com/StanfordSNR/pantheon-tunnel.git
+cd pantheon-tunnel/
+./autogen.sh
+./configure
+make
+sudo make install
+cd ..
+rm -Rf pantheon-tunnel/
+
+# Install Pantheon
 git clone https://github.com/StanfordSNR/pantheon.git
 cd pantheon
 tools/fetch_submodules.sh
@@ -28,3 +35,4 @@ src/experiments/setup.py --setup --schemes "bbr copa cubic fillp fillp_sheep led
 
 echo "Done"
 date
+touch /local/repository/scripts/pantheon/ready
