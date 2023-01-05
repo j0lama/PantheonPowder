35e5cfdaf1dad6890bb7c750abc19fe599c9ada0
Fadi-B <64134035+Fadi-B@users.noreply.github.com>
2023-01-05 19:12:09 +0100

    Update run_experiment.sh

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,12 +1,16 @@
-cd pantheon
+cd pantheon/DIR
 
-src/experiments/setup.py --setup --schemes "sprout sprout-ewma sprout-ma sprout-sewma"
+rm *
+
+cd ..
+
+#sudo src/experiments/setup.py --setup --schemes "sprout sprout-ewma sprout-ma sprout-sewma"
 
 
-#src/experiments/test.py local --schemes "sprout sprout-ewma cubic" --uplink-trace /usr/share/mahimahi/traces/ATT-LTE-driving.up --downlink-trace /usr/share/mahimahi/traces/ATT-LTE-driving.down --data-dir DIR
-src/experiments/test.py local --schemes "sprout-ewma sprout-ma" --uplink-trace /usr/share/mahimahi/traces/TMobile-LTE-driving.up --downlink-trace /usr/share/mahimahi/traces/TMobile-LTE-driving.down --data-dir DIR
+#src/experiments/test.py local --schemes "sprout sprout-ewma cubic" --uplink-trace /usr/share/mahimahi/traces/ATT-LTE-driving.up --downlink-trace /usr/share/mahimah$
+src/experiments/test.py local --schemes "sprout sprout-ma cubic" --uplink-trace /usr/share/mahimahi/traces/TMobile-LTE-driving.up --downlink-trace /usr/share/mahima$
 
-#src/experiments/test.py local --schemes "sprout sprout-ewma cubic" --uplink-trace /usr/share/mahimahi/traces/TMobile-UMTS-driving.up --downlink-trace /usr/share/mahimahi/traces/TMobile-UMTS-driving.down --data-dir DIR
+#src/experiments/test.py local --schemes "sprout sprout-ma cubic" --uplink-trace /usr/share/mahimahi/traces/TMobile-UMTS-driving.up --downlink-trace /usr/share/mahi$
 
 
 src/analysis/analyze.py --data-dir DIR
