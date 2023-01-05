17619d7d47e27cdc0976b49e6c79e0d156f2167b
Fadi-B <64134035+Fadi-B@users.noreply.github.com>
2023-01-05 19:12:55 +0100

    Update setup_framework.sh

--- scripts/setup_framework.sh
+++ scripts/setup_framework.sh
@@ -3,4 +3,14 @@
 cd pantheon
 
 sudo sysctl -w net.ipv4.ip_forward=1
-src/experiments/setup.py --schemes "cubic sprout vivace verus copa tcp-beta"
+
+cd src/wrappers/
+
+chmod 755 sprout-ewma.py
+chmod 755 sprout-ma.py
+chmod 755 sprout-sewma.py
+
+cd ..
+cd ..
+
+src/experiments/setup.py --schemes "cubic sprout vivace verus copa"
