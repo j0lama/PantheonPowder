1ca0eccc4b07710dfa0e05c003d81f1945358f4d
Fadi-B <64134035+Fadi-B@users.noreply.github.com>
2023-01-26 20:21:47 +0100

    Adding sprout-oracle to permission list

--- scripts/setup_framework.sh
+++ scripts/setup_framework.sh
@@ -9,6 +9,7 @@ cd src/wrappers/
 chmod 755 sprout-ewma.py
 chmod 755 sprout-ma.py
 chmod 755 sprout-sewma.py
+chmod 755 sprout-oracle.py
 
 cd ..
 cd ..
@@ -16,5 +17,6 @@ cd ..
 chmod -R 755 third_party/sprout-ewma/
 chmod -R 755 third_party/sprout-ma/
 chmod -R 755 third_party/sprout-sewma/
+chmod -R 755 third_party/sprout-oracle/
 
 src/experiments/setup.py --schemes "cubic sprout vivace verus copa"
