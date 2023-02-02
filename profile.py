150bd1d8454778c9b785aa73dd1179009bf6d43a
Jon Larrea <jon.larrea@ed.ac.uk>
2023-02-02 10:57:17 +0100

    Trying with ubuntu 22

--- profile.py
+++ profile.py
@@ -41,7 +41,8 @@ rspec.addTour(tour)
 
 # Machine
 machine = rspec.RawPC("pantheon")
-machine.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+#machine.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+machine.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-BETA'
 machine.hardware_type = params.Hardware
 machine.addService(PG.Execute(shell="bash", command="/local/repository/scripts/setup_pantheon.sh"))
 
