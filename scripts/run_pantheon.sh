43adf745c7d5da086973999bc688169bdbb32602
j0lama <jon_larrea@hotmail.com>
2023-02-20 14:59:13 +0000

    Adding support for custom oracle traces

--- scripts/run_pantheon.sh
+++ scripts/run_pantheon.sh
@@ -6,7 +6,7 @@
 CC_SCHEMES="bbr oracle" #copa verus
 
 if [ "$#" -gt 2 ] || [ "$#" -lt 1 ]; then
-    echo "USE: $0 <Trace (PCAP or Pantheon)> <Destination IP (Only for PCAP)>"
+    echo "USE: $0 <Trace (PCAP or Pantheon)> <Destination IP (Only for PCAP)> <Oracle trace (optional)>"
     echo "Example: ./run_pantheon.sh /local/repository/scripts/example.pcap 155.98.38.41"
     echo "Example: ./run_pantheon.sh /local/repository/scripts/4gbusserver.pcap 155.98.38.105"
     echo "Example: ./run_pantheon.sh /usr/share/mahimahi/traces/TMobile-LTE-driving.up"
@@ -25,25 +25,35 @@ mkdir -p "tmp_pantheon_traces/"
 # Get trace extension
 ARG1=$1
 EXTENSION="${ARG1##*.}"
-if [ "$EXTENSION" == "pcap" ] && [ "$#" -ne 2 ]; then # PCAP only: show error
-    echo "USE: $0 <Trace PCAP> <Destination IP>"
+if [ "$EXTENSION" == "pcap" ] && [ "$#" -lt 2 ]; then # PCAP only: show error
+    echo "USE: $0 <Trace PCAP> <Destination IP> <Oracle trace (optional)>"
     echo "Example: ./run_pantheon.sh /local/repository/scripts/example.pcap 155.98.38.41"
     exit 1
-elif [ "$EXTENSION" == "pcap" ] && [ "$#" -eq 2 ]; then # PCAP + IP: generate pantheon trace from pcap
+elif [ "$EXTENSION" == "pcap" ] && [ "$#" -eq 2 ]; then # PCAP + IP: generate pantheon trace from pcap and generate oracle trace
     echo "Processing PCAP trace..."
     # Generating Pantheon trace out of a PCAP trace
     PCAP_BASENAME=$(basename -- "$1")
     PANTHEON_TRACE=${PCAP_BASENAME%.*}.x
     /local/repository/scripts/pcap_to_pantheon.py $1 $2 $BASE_DIR/tmp_pantheon_traces/$PANTHEON_TRACE
+    # Generate oracle trace
+    /local/repository/oracle_bbr/generate_oracle_trace.py $BASE_DIR/tmp_pantheon_traces/$PANTHEON_TRACE /local/repository/oracle_bbr/oracle.trace
+elif [ "$EXTENSION" == "pcap" ] && [ "$#" -eq 3 ]; then # PCAP + IP + Oracle trace: generate pantheon trace from pcap
+    echo "Processing PCAP trace..."
+    # Generating Pantheon trace out of a PCAP trace
+    PCAP_BASENAME=$(basename -- "$1")
+    PANTHEON_TRACE=${PCAP_BASENAME%.*}.x
+    /local/repository/scripts/pcap_to_pantheon.py $1 $2 $BASE_DIR/tmp_pantheon_traces/$PANTHEON_TRACE
+    # Copy Oracle trace to destination
+    cp $3 /local/repository/oracle_bbr/oracle.trace
 else # Pantheon trace
     echo "Processing Pantheon trace..."
     PCAP_BASENAME=$(basename -- "$1")
     PANTHEON_TRACE=${PCAP_BASENAME%.*}.x
     cp $1 $BASE_DIR/tmp_pantheon_traces/$PANTHEON_TRACE
+    # Generate oracle trace
+    /local/repository/oracle_bbr/generate_oracle_trace.py $BASE_DIR/tmp_pantheon_traces/$PANTHEON_TRACE /local/repository/oracle_bbr/oracle.trace
 fi
 
-# Generate oracle trace
-/local/repository/oracle_bbr/generate_oracle_trace.py $BASE_DIR/tmp_pantheon_traces/$PANTHEON_TRACE /local/repository/oracle_bbr/oracle.trace
 
 # Compile and install BBR Oracle
 cd /local/repository/oracle_bbr/
